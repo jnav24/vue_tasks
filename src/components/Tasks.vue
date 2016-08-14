4885cadafbbe9c124f8736b0cf89b18834d102e5
Justin Navarro <jnavarro@qinteractive.com>
2016-08-14 14:22:34 -0400

    basic task setup

---
+++ src/components/Tasks.vue
@@ -0,0 +1,24 @@
+<template>
+  <ul>
+    <li :class="{'completed':task.completed}" v-for="task in tasks" @click="task.completed = ! task.completed">{{ task.name }}</li>
+  </ul>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        tasks: [
+          { name: 'Check budget', completed: false },
+          { name: 'Pick up milk', completed: false },
+          { name: 'Write some code', completed: true }
+        ]
+      }
+    }
+  }
+</script>
+<style>
+    .completed {
+        text-decoration: line-through;
+    }
+</style>
